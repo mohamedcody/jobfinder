aac46c95a27a58973ff6862b38dda14175a03d76
Mohamed Saad <mohamedcody18@gmail.com>
2026-03-08 07:39:49 +0200

    add

--- backend/src/main/java/com/example/jobfinder/repository/OtpCodeRepository.java
+++ backend/src/main/java/com/example/jobfinder/repository/OtpCodeRepository.java
@@ -5,6 +5,8 @@ import jobfinder.model.entity.OtpCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OtpCodeRepository extends JpaRepository<OtpCode, Long > {
 
